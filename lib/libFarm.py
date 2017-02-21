b45851b58929cf1d699750840effc348753fb4b6
ch-tseng <ch.tseng@sunplusit.com>
2017-02-21 03:40:26 -0500

    update

--- lib/libFarm.py
+++ lib/libFarm.py
@@ -44,14 +44,30 @@ class WEEDS:
         (T_weeds_a, thresh_weeds_a) = cv2.threshold(A, a_threshold, 255, cv2.THRESH_BINARY)
         imgRGB = cv2.merge([zeros, thresh_weeds_b, thresh_weeds_a])
         self.image = imgRGB
-        #imgGray = cv2.cvtColor(imgRGB, cv2.COLOR_BGR2GRAY)
 
+    def countPlantArea(self):
+        image = self.image
+        width = image.shape[1]
+        height = image.shape[0]
+        greenArea = 0.0
+        
+        for pixel_w in range(0, width, 1):
+            for pixel_h in range(0, height, 1):
+                (b, g, r) = image[pixel_h, pixel_w]
+                if((b+g+r)>0):
+                    greenArea += 1     
+
+        plantArea = greenArea / (width*height)
+        ratio = str(int(plantArea * 100)) + "%"
+        cv2.putText(image, ratio, (width-150, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 1, 126), 5)
+        self.image = image
 
     def extractWeeds(self, threshold=80, channel="B"):
         zeros = np.zeros(self.image.shape[:2], dtype = "uint8")
 
         imgLAB = cv2.cvtColor(self.image, self.colorSpace)
         (L, A, B) = cv2.split(imgLAB)
+
         if(channel=="B"):
             selectedChannel = B
         elif(channel=="L"):
